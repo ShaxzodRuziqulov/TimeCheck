58becbe03f59ad3788c7becc4763e64aee418175
Shaxzod <janobprizident@gmail.com>
2025-06-04 12:36:10 +0500

    for user update methode

---
+++ src/main/java/com/example/timecheck/web/rest/UserProfileResource.java
@@ -0,0 +1,36 @@
+package com.example.timecheck.web.rest;
+
+import com.example.timecheck.service.UserService;
+import com.example.timecheck.service.dto.UserDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserProfileResource {
+    private final UserService userService;
+
+    public UserProfileResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PutMapping("/update-profile/{id}")
+    @PreAuthorize("hasAnyAuthority('ROLE_USER', 'ROLE_ADMIN')")
+    public ResponseEntity<?> update(@RequestBody UserDto userDto, @PathVariable Long id) throws URISyntaxException {
+        if (!userDto.getId().equals(id) && userDto.getId() != 0) {
+            return ResponseEntity.badRequest().body("invalid id");
+        }
+        UserDto result = userService.update(userDto);
+        return ResponseEntity.ok().body(result);
+    }
+    @GetMapping("/activeUser")
+    public ResponseEntity<?> findAllUsers() {
+        List<UserDto> result = userService.allActiveUsers();
+        return ResponseEntity.ok(result);
+    }
+
+}
